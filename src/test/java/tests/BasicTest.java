f1e69c8d109a220765b190f1b1baaed52c416b11
Svetlana <svetlana_radisic@yahoo.com>
2026-02-09 22:00:01 +0100

    Update dependencies, remove WebDriverManager from setup

--- src/test/java/tests/BasicTest.java
+++ src/test/java/tests/BasicTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import helper.Helper;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -34,7 +33,6 @@ public abstract class BasicTest {
 
     @BeforeClass
     public void setup() {
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
